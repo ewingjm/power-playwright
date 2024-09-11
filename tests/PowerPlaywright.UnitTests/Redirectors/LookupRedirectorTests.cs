77ac8f6c86423bd9934b34c2159c394b59d3351c
Max Ewing <max.ewing@capgemini.com>
2024-09-11 10:25:22 +0100

    test: redirector unit tests

---
+++ tests/PowerPlaywright.UnitTests/Redirectors/LookupRedirectorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace PowerPlaywright.UnitTests.Redirectors
+{
+    using PowerPlaywright.Framework.Controls.Pcf;
+    using PowerPlaywright.Framework.Redirectors;
+    using PowerPlaywright.Strategies.Redirectors;
+
+    /// <summary>
+    /// Tests for the <see cref="ReadOnlyGridRedirector"/> class.
+    /// </summary>
+    public class LookupRedirectorTests
+    {
+        private LookupRedirector redirector;
+
+        /// <summary>
+        /// Sets up each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            this.redirector = new LookupRedirector();
+        }
+
+        /// <summary>
+        /// Tests that the right control is returned based on the <see cref="AppToggles.ModernizationOptOut"/> flag.
+        /// </summary>
+        /// <param name="modernizationOptOut">The <see cref="AppToggles.ModernizationOptOut"/> flag.</param>
+        /// <returns>The redirected type.</returns>
+        [TestCase(false, ExpectedResult = typeof(ISimpleLookupControl))]
+        [TestCase(true, ExpectedResult = typeof(ISimpleLookupControl))]
+        public Type Redirect_ModernizationOptOutSet_ReturnsCorrespondingLookupInterface(bool modernizationOptOut)
+        {
+            return this.redirector.Redirect(new ControlRedirectionInfo(new AppToggles { ModernizationOptOut = modernizationOptOut }));
+        }
+    }
+}
---
+++ tests/PowerPlaywright.UnitTests/Redirectors/ReadOnlyGridRedirectorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace PowerPlaywright.UnitTests.Redirectors
+{
+    using PowerPlaywright.Framework.Controls.Pcf;
+    using PowerPlaywright.Framework.Redirectors;
+    using PowerPlaywright.Strategies.Redirectors;
+
+    /// <summary>
+    /// Tests for the <see cref="ReadOnlyGridRedirector"/> class.
+    /// </summary>
+    public class ReadOnlyGridRedirectorTests
+    {
+        private ReadOnlyGridRedirector redirector;
+
+        /// <summary>
+        /// Sets up each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            this.redirector = new ReadOnlyGridRedirector();
+        }
+
+        /// <summary>
+        /// Tests that the right control is returned based on the <see cref="AppToggles.ModernizationOptOut"/> flag.
+        /// </summary>
+        /// <param name="modernizationOptOut">The <see cref="AppToggles.ModernizationOptOut"/> flag.</param>
+        /// <returns>The redirected type.</returns>
+        [TestCase(false, ExpectedResult = typeof(IPcfGridControl))]
+        [TestCase(true, ExpectedResult = typeof(IPowerAppsOneGridControl))]
+        public Type Redirect_ModernizationOptOutSet_ReturnsCorrespondingReadOnlyGridInterface(bool modernizationOptOut)
+        {
+            return this.redirector.Redirect(new ControlRedirectionInfo(new AppToggles { ModernizationOptOut = modernizationOptOut }));
+        }
+    }
+}
