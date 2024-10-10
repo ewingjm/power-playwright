a4bb74784028a6a6c924bd60c6617c6604f6f6ce
Max Ewing <max.ewing@capgemini.com>
2024-10-10 12:29:38 +0100

    refactor: remove unneeded exception constructors

--- src/PowerPlaywright/PowerPlaywrightException.cs
+++ src/PowerPlaywright/PowerPlaywrightException.cs
@@ -12,36 +12,9 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="PowerPlaywrightException"/> class.
         /// </summary>
-        public PowerPlaywrightException()
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PowerPlaywrightException"/> class.
-        /// </summary>
         /// <param name="message">The message.</param>
         public PowerPlaywrightException(string message)
             : base(message)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PowerPlaywrightException"/> class.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="innerException">The inner exception.</param>
-        public PowerPlaywrightException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PowerPlaywrightException"/> class.
-        /// </summary>
-        /// <param name="info">The serialization info.</param>
-        /// <param name="context">The streaming context.</param>
-        protected PowerPlaywrightException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
