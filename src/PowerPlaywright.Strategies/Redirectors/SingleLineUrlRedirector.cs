980f00cfd44aa60ebe7d9ee6ceaf604f77f5e4a2
Max Ewing <max.ewing@capgemini.com>
2025-03-20 16:42:50 +0000

    feat: add single line url control class redirector

---
+++ src/PowerPlaywright.Strategies/Redirectors/SingleLineUrlRedirector.cs
@@ -0,0 +1,36 @@
+﻿namespace PowerPlaywright.Strategies.Redirectors
+{
+    using Microsoft.Extensions.Logging;
+    using PowerPlaywright.Framework.Controls.Pcf;
+    using PowerPlaywright.Framework.Controls.Pcf.Classes;
+    using PowerPlaywright.Framework.Redirectors;
+    using System;
+
+    /// <summary>
+    /// Redirects requests for an <see cref="ISingleLineUrl"/> control.
+    /// </summary>
+    public class SingleLineUrlRedirector : ControlRedirector<ISingleLineUrl>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SingleLineUrlRedirector"/> class.
+        /// </summary>
+        /// <param name="infoProvider">The info provider.</param>
+        /// <param name="logger">The logger.</param>
+        public SingleLineUrlRedirector(IRedirectionInfoProvider<RedirectionInfo> infoProvider, ILogger logger = null)
+            : base(infoProvider, logger)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override Type GetTargetControlType(RedirectionInfo redirectionInfo)
+        {
+            if (redirectionInfo.ActiveReleaseChannel == ReleaseChannel.SemiAnnualChannel && !redirectionInfo.IsNewLookEnabled)
+            {
+                return typeof(IUrlControl);
+            }
+
+            // TODO: Create new look implementation.
+            throw new NotSupportedException("The single-line URL control class is not supported on the new look.");
+        }
+    }
+}
