42414c67e86b8907565d3bda5e8b90776f53efab
Max Ewing <max.ewing@capgemini.com>
2025-03-20 16:41:55 +0000

    refactor: fix analyser warnings

--- src/PowerPlaywright.Strategies/Controls/Pcf/UrlControl.cs
+++ src/PowerPlaywright.Strategies/Controls/Pcf/UrlControl.cs
@@ -6,27 +6,41 @@
     using PowerPlaywright.Framework.Controls.Pcf;
     using PowerPlaywright.Framework.Pages;
     using PowerPlaywright.Framework.Controls;
+    using PowerPlaywright.Strategies.Extensions;
 
+    /// <summary>
+    /// A control strategy for the <see cref="IUrlControl"/>.
+    /// </summary>
     [PcfControlStrategy(0, 0, 0)]
-    public class UrlContol : PcfControl, IUrlControl
+    public class UrlControl : PcfControl, IUrlControl
     {
         private readonly ILocator urlInput;
 
-        public UrlContol(string name, IAppPage appPage, IControl parent = null) : base(name, appPage, parent)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleLookupControl"/> class.
+        /// </summary>
+        /// <param name="name">The name given to the control.</param>
+        /// <param name="appPage">The app page.</param>
+        /// <param name="parent">The parent control.</param>
+        public UrlControl(string name, IAppPage appPage, IControl parent = null)
+            : base(name, appPage, parent)
         {
             this.urlInput = this.Container.Locator("input");
         }
 
+        /// <inheritdoc/>
         public async Task<string> GetValueAsync()
         {
-            return await this.urlInput.InputValueAsync();
+            return await this.urlInput.InputValueOrNullAsync();
         }
 
+        /// <inheritdoc/>
         public async Task SetValueAsync(string value)
         {
             await this.urlInput.FillAsync(value);
         }
 
+        /// <inheritdoc/>
         protected override ILocator GetRoot(ILocator context)
         {
             return context.Locator($"div[data-lp-id*='MscrmControls.FieldControls.UrlControl|{this.Name}.fieldControl|']");
