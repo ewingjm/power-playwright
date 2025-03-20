a45f8b9cc3e6ad104e502b7fc9ea497aaa818ea0
Max Ewing <max.ewing@capgemini.com>
2025-03-20 16:43:01 +0000

    feat: add InputValueOrNullAsync extension

---
+++ src/PowerPlaywright.Strategies/Extensions/ILocatorExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace PowerPlaywright.Strategies.Extensions
+{
+    using Microsoft.Playwright;
+    using System.Threading.Tasks;
+
+    internal static class ILocatorExtensions
+    {
+        private const string PowerAppsInputPlaceholderValue = "---";
+
+        /// <summary>
+        /// Returns the value returned by <see cref="ILocator.InputValueAsync(LocatorInputValueOptions)"/> but transforms placeholder values (e.g. '---') to null.
+        /// </summary>
+        /// <param name="locator">The locator.</param>
+        /// <param name="options">The options.</param>
+        /// <returns></returns>
+        internal static async Task<string> InputValueOrNullAsync(this ILocator locator, LocatorInputValueOptions options = null)
+        {
+            var value = await locator.InputValueAsync(options);
+
+            return value != PowerAppsInputPlaceholderValue ? value : null;
+        }
+    }
+}
