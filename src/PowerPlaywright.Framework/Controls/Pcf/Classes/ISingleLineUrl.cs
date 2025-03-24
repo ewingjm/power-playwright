c02c9394b5fe539d1ba7434fb2d7fb2346095809
Max Ewing <max.ewing@capgemini.com>
2025-03-24 11:23:31 +0000

    refactor: fix ISingleLineUrl comments

--- src/PowerPlaywright.Framework/Controls/Pcf/Classes/ISingleLineUrl.cs
+++ src/PowerPlaywright.Framework/Controls/Pcf/Classes/ISingleLineUrl.cs
@@ -3,19 +3,19 @@
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Lookup control class.
+    /// Single-line text (URL) control class.
     /// </summary>
     public interface ISingleLineUrl : IPcfControl
     {
         /// <summary>
-        /// Sets the value of the lookup.
+        /// Sets the value of the single-line text (URL) control.
         /// </summary>
         /// <param name="value">The value.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task SetValueAsync(string value);
 
         /// <summary>
-        /// Gets the value of the lookup.
+        /// Gets the value of the single-line text (URL) control.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task<string> GetValueAsync();
