3275bb8d6ade389e81fd50c203e55407aa592b6b
Mark Cunningham <mark_cunningham@outlook.com>
2025-04-06 08:07:31 +0100

    Adding the placeholders for the next set of controls so they are merged ahead of the next PR. As some of the namespaces are changing. Using the naming strategy to map MSFT's

---
+++ src/PowerPlaywright.Framework/Controls/Pcf/IOptionSetControl.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerPlaywright.Framework.Controls.Pcf
+{
+    using PowerPlaywright.Framework.Controls.Pcf.Attributes;
+    using PowerPlaywright.Framework.Controls.Pcf.Classes;
+
+    /// <summary>
+    /// An interface for the MscrmControls.FieldControls.OptionSet control.
+    /// </summary>
+    [PcfControl("PowerApps.CoreControls.OptionSetControl")]
+    public interface IOptionSetControl
+    {
+    }
+}
