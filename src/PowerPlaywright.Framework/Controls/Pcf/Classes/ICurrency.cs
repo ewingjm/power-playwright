74118a0f417731b7a3402af39c4b7e84953a81d2
Mark Cunningham <mark_cunningham@outlook.com>
2025-04-15 17:03:50 +0100

    Update src/PowerPlaywright.Framework/Controls/Pcf/Classes/ICurrency.cs
    Co-authored-by: Max Ewing <max.ewing@outlook.com>

--- src/PowerPlaywright.Framework/Controls/Pcf/Classes/ICurrency.cs
+++ src/PowerPlaywright.Framework/Controls/Pcf/Classes/ICurrency.cs
@@ -8,7 +8,7 @@
     public interface ICurrency : IPcfControl
     {
         /// <summary>
-        /// Sets the value of the (Currency) control.
+        /// Sets the value of the Currency control.
         /// </summary>
         /// <param name="value">The value.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
