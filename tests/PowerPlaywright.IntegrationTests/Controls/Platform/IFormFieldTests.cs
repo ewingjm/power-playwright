857443aa40f1c5687097f0732d08eea696f62646
Max Ewing <max.ewing@capgemini.com>
2025-06-05 18:03:00 +0100

    test: fix broken tests

--- tests/PowerPlaywright.IntegrationTests/Controls/Platform/IFormFieldTests.cs
+++ tests/PowerPlaywright.IntegrationTests/Controls/Platform/IFormFieldTests.cs
@@ -1,10 +1,10 @@
 ﻿namespace PowerPlaywright.IntegrationTests.Controls.Platform
 {
     using System.Threading.Tasks;
-    using Bogus;
     using PowerPlaywright.Framework.Controls.Pcf;
     using PowerPlaywright.Framework.Controls.Pcf.Classes;
     using PowerPlaywright.Framework.Controls.Platform;
+    using PowerPlaywright.Framework.Extensions;
     using PowerPlaywright.TestApp.Model;
     using PowerPlaywright.TestApp.Model.Fakers;
 
@@ -21,7 +21,7 @@
         public async Task PcfControl_Always_ReturnsPcfControlInstance()
         {
             var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField<ISingleLineUrl>(nameof(pp_Record.pp_singlelineoftexturl));
+            var control = form.GetField<ISingleLineUrl>(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.Control, Is.Not.Null);
         }
@@ -33,8 +33,8 @@
         [Test]
         public async Task IsMandatoryAsync_MandatoryField_ReturnsTrue()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_singlelineoftexttext));
+            var form = await this.SetupFormScenarioAsync(withMandatoryField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.IsMandatoryAsync, Is.True);
         }
@@ -46,8 +46,8 @@
         [Test]
         public async Task IsMandatoryAsync_NonMandatoryField_ReturnsFalse()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_currency));
+            var form = await this.SetupFormScenarioAsync(withOptionalField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.IsMandatoryAsync, Is.False);
         }
@@ -59,8 +59,8 @@
         [Test]
         public async Task IsDisabledAsync_DisabledField_ReturnsTrue()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_singlelineoftexttext));
+            var form = await this.SetupFormScenarioAsync(withDisabledField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.IsDisabledAsync, Is.True);
         }
@@ -72,8 +72,8 @@
         [Test]
         public async Task IsDisabledAsync_NonDisabledField_ReturnsFalse()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_currency));
+            var form = await this.SetupFormScenarioAsync(withOptionalField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.IsMandatoryAsync, Is.False);
         }
@@ -85,8 +85,8 @@
         [Test]
         public async Task IsVisibleAsync_ControlIsVisible_ReturnsTrue()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_singlelineoftexturl));
+            var form = await this.SetupFormScenarioAsync(withVisibleField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.IsVisibleAsync, Is.True);
         }
@@ -98,8 +98,8 @@
         [Test]
         public async Task IsVisibleAsync_ControlIsHidden_ReturnsFalse()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_singlelineoftexturl));
+            var form = await this.SetupFormScenarioAsync(withHiddenField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
             Assert.That(control.IsVisibleAsync, Is.False);
         }
@@ -111,10 +111,10 @@
         [Test]
         public async Task GetLabelAsync_LabelIsVisible_ReturnsLabelText()
         {
-            var form = await this.SetupFormScenarioAsync();
-            var control = form.GetField(nameof(pp_Record.pp_singlelineoftexturl));
+            var form = await this.SetupFormScenarioAsync(withVisibleField: pp_Record.Forms.Information.WholeNumberNone);
+            var control = form.GetField(pp_Record.Forms.Information.WholeNumberNone);
 
-            Assert.That(control.GetLabelAsync, Is.EqualTo("Single line of text (URL)"));
+            Assert.That(control.GetLabelAsync, Is.EqualTo("Whole number (none)"));
         }
 
         /// <summary>
@@ -133,12 +133,50 @@
         /// <summary>
         /// Sets up a form scenario.
         /// </summary>
+        /// <param name="withEditableField">An optional field to setup as editable on the form.</param>
+        /// <param name="withDisabledField">An optional field to set up as disabled on the form.</param>
+        /// <param name="withOptionalField">An optional field to set up as non-mandatory on the form.</param>
+        /// <param name="withMandatoryField">An optional field to set up as mandatory on the form.</param>
+        /// <param name="withHiddenField">An optional field to set up as hidden on the form.</param>
+        /// <param name="withVisibleField">An optional field to set up as visible on the form.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation. The task result contains the initialized <see cref="IUrlControl"/>.</returns>
-        private async Task<IMainForm> SetupFormScenarioAsync()
+        private async Task<IMainForm> SetupFormScenarioAsync(string? withEditableField = null, string? withDisabledField = null, string? withOptionalField = null, string? withMandatoryField = null, string? withHiddenField = null, string? withVisibleField = null)
         {
             var record = new RecordFaker();
 
             var recordPage = await this.LoginAndNavigateToRecordAsync(record.Generate());
+
+            if (withEditableField != null)
+            {
+                await recordPage.Page.EvaluateAsync("field => Xrm.Page.getControl(field).setDisabled(false)", withEditableField);
+            }
+
+            if (withDisabledField != null)
+            {
+                await recordPage.Page.EvaluateAsync("field => Xrm.Page.getControl(field).setDisabled(true)", withDisabledField);
+            }
+
+            if (withOptionalField != null)
+            {
+                await recordPage.Page.EvaluateAsync("field => Xrm.Page.getControl(field).getAttribute().setRequiredLevel('none')", withOptionalField);
+            }
+
+            if (withMandatoryField != null)
+            {
+                await recordPage.Page.EvaluateAsync("field => Xrm.Page.getControl(field).getAttribute().setRequiredLevel('required')", withMandatoryField);
+            }
+
+            if (withHiddenField != null)
+            {
+                await recordPage.Page.EvaluateAsync("field => Xrm.Page.getControl(field).setVisible(false)", withHiddenField);
+            }
+
+            if (withVisibleField != null)
+            {
+                await recordPage.Page.EvaluateAsync("field => Xrm.Page.getControl(field).setVisible(true)", withVisibleField);
+            }
+
+            await recordPage.Page.WaitForAppIdleAsync();
 
             return recordPage.Form;
         }
