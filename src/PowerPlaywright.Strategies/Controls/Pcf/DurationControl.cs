35b9edbb07585c7b271de045c9fcff4bc28e983a
Mark Cunningham <mark_cunningham@outlook.com>
2025-06-07 05:27:10 +0100

    Closes #13

--- src/PowerPlaywright.Strategies/Controls/Pcf/DurationControl.cs
+++ src/PowerPlaywright.Strategies/Controls/Pcf/DurationControl.cs
@@ -8,38 +8,42 @@
     using PowerPlaywright.Framework.Controls;
     using PowerPlaywright.Strategies.Extensions;
     using PowerPlaywright.Framework;
+    using System.Threading;
+    using PowerPlaywright.Framework.Extensions;
 
     /// <summary>
-    /// A control strategy for the <see cref="IWholeNumberControl"/>.
+    /// A control strategy for the <see cref="IDurationControl"/>.
     /// </summary>
     [PcfControlStrategy(0, 0, 0)]
-    public class WholeNumberControl : PcfControlInternal, IWholeNumberControl
+    public class DurationControl : PcfControlInternal, IDurationControl
     {
         private readonly ILocator input;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="WholeNumberControl"/> class.
+        /// Initializes a new instance of the <see cref="DurationControl"/> class.
         /// </summary>
         /// <param name="name">The name given to the control.</param>
         /// <param name="appPage">The app page.</param>
         /// <param name="infoProvider">The info provider.</param>
         /// <param name="parent">The parent control.</param>
-        public WholeNumberControl(string name, IAppPage appPage, IEnvironmentInfoProvider infoProvider, IControl parent = null)
+        public DurationControl(string name, IAppPage appPage, IEnvironmentInfoProvider infoProvider, IControl parent = null)
             : base(name, appPage, infoProvider, parent)
         {
-            this.input = this.Container.Locator("input");
+            this.input = this.Container.GetByRole(AriaRole.Combobox);
         }
 
         /// <inheritdoc/>
-        public async Task<int?> GetValueAsync()
+        public async Task<string> GetValueAsync()
         {
-            return await this.input.InputValueOrNullAsync<int?>();
+            return await this.input.InputValueOrNullAsync();
         }
 
         /// <inheritdoc/>
-        public async Task SetValueAsync(int? value)
+        public async Task SetValueAsync(string value)
         {
-            await this.input.FillAsync(value.ToString());
+            await input.ClearAsync();
+            await this.input.FillAsync(value);
+            await AppPage.Page.WaitForAppIdleAsync();
         }
     }
 }
---
+++ tests/PowerPlaywright.IntegrationTests/Controls/Pcf/IDurationTests.cs
@@ -0,0 +1,90 @@
+﻿namespace PowerPlaywright.IntegrationTests.Controls.Pcf
+{
+    using Bogus;
+    using PowerPlaywright.Framework.Controls.Pcf.Classes;
+    using PowerPlaywright.TestApp.Model;
+    using PowerPlaywright.TestApp.Model.Fakers;
+
+    /// <summary>
+    /// Tests for the <see cref="IDuration"/> control class.
+    /// </summary>
+    public class IDurationTests : IntegrationTests
+    {
+        private Faker faker;
+
+        /// <summary>
+        /// Sets up the test dependencies.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.faker = new Faker();
+        }
+
+        /// <summary>
+        /// Tests that <see cref="IDuration.GetValueAsync"/> returns the value when the value has been set.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task GetValueAsync_ContainsValue_ReturnsValue()
+        {
+            var expectedValue = 5;
+            var durationControl = await this.SetupDurationScenarioAsync(withValue: expectedValue);
+
+            Assert.That(durationControl.GetValueAsync, Is.EqualTo($"{expectedValue} minutes"));
+        }
+
+        /// <summary>
+        /// Tests that <see cref="IDuration.SetValueAsync(string)"/> sets the value.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task SetValueAsync_DoesNotContainValue_SetsValue()
+        {
+            var durationControl = await this.SetupDurationScenarioAsync(withNoValue: true);
+            var expectedValue = "5 minutes";
+
+            await durationControl.SetValueAsync(expectedValue);
+
+            Assert.That(durationControl.GetValueAsync, Is.EqualTo(expectedValue));
+        }
+
+        /// <summary>
+        /// Tests that <see cref="IDuration.SetValueAsync(string)"/> replaces the value when the control already contains a value.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task SetValueAsync_ContainsValue_ReplacesValue()
+        {
+            var durationControl = await this.SetupDurationScenarioAsync();
+            var expectedValue = "5 minutes";
+
+            await durationControl.SetValueAsync(expectedValue);
+
+            Assert.That(durationControl.GetValueAsync, Is.EqualTo(expectedValue));
+        }
+
+        /// <summary>
+        /// Sets up a control scenario for testing by creating a record with a specified or generated value.
+        /// </summary>
+        /// <param name="withValue">An optional value to set in the record. If null, a random value will be generated.</param>
+        /// <param name="withNoValue">Whether to set the choice to null. Defaults to false.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation. The task result contains the initialized <see cref="IDuration"/>.</returns>
+        private async Task<IDuration> SetupDurationScenarioAsync(int? withValue = null, bool withNoValue = false)
+        {
+            var record = new RecordFaker();
+
+            if (withNoValue)
+            {
+                record.RuleFor(x => x.pp_wholenumberduration, f => null!);
+            }
+            else if (withValue is not null)
+            {
+                record.RuleFor(x => x.pp_wholenumberduration, withValue);
+            }
+
+            var recordPage = await this.LoginAndNavigateToRecordAsync(record.Generate());
+            return recordPage.Form.GetField<IDuration>(nameof(pp_Record.pp_wholenumberduration)).Control;
+        }
+    }
+}
