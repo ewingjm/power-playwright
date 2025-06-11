41bd25dcefdeef59779342c1ea2311a6495d68b8
Max Ewing <max.ewing@capgemini.com>
2025-06-11 12:55:58 +0100

    refactor: remove unnecessary usings

--- src/PowerPlaywright.Strategies/Controls/Pcf/PowerAppsOneGridControl.cs
+++ src/PowerPlaywright.Strategies/Controls/Pcf/PowerAppsOneGridControl.cs
@@ -1,8 +1,6 @@
 ﻿namespace PowerPlaywright.Strategies.Controls.Pcf
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
     using Microsoft.Playwright;
