ad6d6ecb664a6efcef0d2567475b3fbd739cb95b
Max Ewing <max.ewing@capgemini.com>
2025-06-12 15:31:34 +0100

    fix: text controls

---
+++ src/PowerPlaywright.Framework/Controls/Pcf/ITextBoxControl.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerPlaywright.Framework.Controls.Pcf
+{
+    using PowerPlaywright.Framework.Controls.Pcf.Attributes;
+    using PowerPlaywright.Framework.Controls.Pcf.Classes;
+
+    /// <summary>
+    /// An interface for the MscrmControls.FieldControls.TextBox control.
+    /// </summary>
+    [PcfControl("MscrmControls.FieldControls.TextBoxControl")]
+    public interface ITextBoxControl : ISingleLineText, ISingleLineTextArea
+    {
+    }
+}
