578608ccdad71a7ad8b5799871b68623cea030e0
Max Ewing <max.ewing@capgemini.com>
2025-06-13 10:58:55 +0100

    fix: missing namespace

--- src/PowerPlaywright.Strategies/Controls/Pcf/PowerAppsOneGridControl.cs
+++ src/PowerPlaywright.Strategies/Controls/Pcf/PowerAppsOneGridControl.cs
@@ -1,5 +1,6 @@
 ﻿namespace PowerPlaywright.Strategies.Controls.Pcf
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
