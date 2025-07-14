20fc0b179c311da3d623567c668226c1b4f23fc8
Mark Cunningham <mark_cunningham@outlook.com>
2025-07-14 17:40:11 +0100

    feat: multiple lines of text (#87)
    * solution for the multi line text control
    
    * Use Bogus Lines

---
+++ src/PowerPlaywright.Strategies/Redirectors/MultiLineTextRedirector.cs
@@ -0,0 +1,35 @@
+﻿namespace PowerPlaywright.Strategies.Redirectors
+{
+    using System;
+    using Microsoft.Extensions.Logging;
+    using PowerPlaywright.Framework.Controls.Pcf;
+    using PowerPlaywright.Framework.Controls.Pcf.Classes;
+    using PowerPlaywright.Framework.Redirectors;
+
+    /// <summary>
+    /// Redirects requests for an <see cref="IMultiLineText"/> control.
+    /// </summary>
+    public class MultiLineTextRedirector : ControlRedirector<IMultiLineText>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiLineTextRedirector"/> class.
+        /// </summary>
+        /// <param name="infoProvider">The info provider.</param>
+        /// <param name="logger">The logger.</param>
+        public MultiLineTextRedirector(IRedirectionInfoProvider infoProvider, ILogger<MultiLineTextRedirector> logger)
+            : base(infoProvider, logger)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override Type GetTargetControlType(IRedirectionEnvironmentInfo environmentInfo, RedirectionControlInfo controlInfo)
+        {
+            if (!environmentInfo.IsNewLookEnabled)
+            {
+                return typeof(ITextBoxControl);
+            }
+
+            return typeof(ITextInput);
+        }
+    }
+}
