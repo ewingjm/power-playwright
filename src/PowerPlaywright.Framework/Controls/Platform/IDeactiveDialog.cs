1597f93eacedf4a04e9836ce1902e4ff42c238b8
Iain Lomax <iain.lomax@capgemini.com>
2025-10-16 08:15:53 +0100

    feat: add interfaces for platform control deactivate dialog #113

---
+++ src/PowerPlaywright.Framework/Controls/Platform/IDeactiveDialog.cs
@@ -0,0 +1,25 @@
+﻿namespace PowerPlaywright.Framework.Controls.Platform
+{
+    using System.Threading.Tasks;
+    using PowerPlaywright.Framework.Controls.Platform.Attributes;
+
+    /// <summary>
+    /// Deactivate dialog.
+    /// </summary>
+    [PlatformControl]
+    public interface IDeactiveDialog : ISetStateDialog
+    {
+        /// <summary>
+        /// Click the deactivate button.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task DeactivateAsync();
+
+        /// <summary>
+        /// Sets the value of the choice control.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task SetValueAsync(string value);
+    }
+}
---
+++ src/PowerPlaywright.Framework/Controls/Platform/ISetStateDialog.cs
@@ -0,0 +1,48 @@
+﻿namespace PowerPlaywright.Framework.Controls.Platform
+{
+    using System.Threading.Tasks;
+    using PowerPlaywright.Framework.Controls.Platform.Attributes;
+
+    /// <summary>
+    /// Set state dialog.
+    /// </summary>
+    [PlatformControl]
+    public interface ISetStateDialog : IPlatformControl
+    {
+        /// <summary>
+        /// Gets whether the dialog is visible.
+        /// </summary>
+        /// <returns>A boolean indicating whether the dialog is visible.</returns>
+        Task<bool> IsVisibleAsync();
+
+        /// <summary>
+        /// Gets the title of the dialog.
+        /// </summary>
+        /// <returns>The title of the dialog.</returns>
+        Task<string> GetTitleAsync();
+
+        /// <summary>
+        /// Gets the confirmation text of the dialog.
+        /// </summary>
+        /// <returns>The confirmation text of the dialog.</returns>
+        Task<string> GetTextAsync();
+
+        /// <summary>
+        /// Gets the informative statement of for the action the user is about to take.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task<string> GetActionTextAsync();
+
+        /// <summary>
+        /// Close the the dialog.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task CloseAsync();
+
+        /// <summary>
+        /// Cancel the the dialog.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task CancelAsync();
+    }
+}
