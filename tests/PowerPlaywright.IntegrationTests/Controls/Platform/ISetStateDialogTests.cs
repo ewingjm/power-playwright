1d8a4c28c6cc196a3a59411c424baaba87416574
Iain Lomax <iain.lomax@capgemini.com>
2025-10-17 11:20:35 +0100

    test: update integration test scenarios

--- tests/PowerPlaywright.IntegrationTests/Controls/Platform/ISetStateDialogTests.cs
+++ tests/PowerPlaywright.IntegrationTests/Controls/Platform/ISetStateDialogTests.cs
@@ -2,6 +2,7 @@
 {
     using System.Threading.Tasks;
     using Bogus;
+    using PowerPlaywright.Framework;
     using PowerPlaywright.Framework.Controls.Platform;
     using PowerPlaywright.IntegrationTests.Extensions;
     using PowerPlaywright.TestApp.Model;
@@ -55,62 +56,96 @@
         [Test]
         public async Task CancelAsync_DialogIsVisible_Closes()
         {
-            var deactivateDialog = await this.SetupDeactivateDialogScenarioAsync();
+            var dialog = await this.SetupDeactivateDialogScenarioAsync();
 
-            await deactivateDialog.CancelAsync();
+            await dialog.CancelAsync();
 
-            Assert.That(await deactivateDialog.IsVisibleAsync(), Is.False);
+            Assert.That(await dialog.IsVisibleAsync(), Is.False);
         }
-
-        ///// <summary>
-        ///// Tests that <see cref="ISetStateDialog.DeactivateAsync"/> closes the dialog when it is visible.
-        ///// </summary>
-        ///// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        //[Test]
-        //public async Task DeactivateAsync_DialogIsVisible_Closes()
-        //{
-        //    var deactivateDialog = await this.SetupDeactivateDialogScenarioAsync();
-
-        //    await deactivateDialog.DeactivateAsync();
-
-        //    Assert.That(await deactivateDialog.IsVisibleAsync(), Is.False);
-        //}
 
         /// <summary>
         /// Tests that <see cref="ISetStateDialog.DeactivateAsync"/> closes the dialog when it is visible.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        //[Test]
-        //public async Task SetValueAsync_ContainsValue_ReplacesValue()
-        //{
-        //    var deactivateDialog = await this.SetupDeactivateDialogScenarioAsync();
-        //    var option = await deactivateDialog.GetValueAsync();
+        [Test]
+        public async Task DeactivateAsync_DialogIsVisible_Closes()
+        {
+            var dialog = await this.SetupDeactivateDialogScenarioAsync();
 
-        //    var expectedValue = pp_record_statuscode.Inactive.ToDisplayName();
+            await dialog.DeactivateAsync();
 
-        //    await deactivateDialog.SetValueAsync(expectedValue);
-
-        //    Assert.That(await deactivateDialog.GetValueAsync(), Is.EqualTo(expectedValue));
-
-
-        //    //var choiceControl = await this.SetupChoiceScenarioAsync();
-        //    //var expectedValue = this.faker.PickRandom<pp_record_pp_choice>().ToDisplayName();
-
-        //    //await choiceControl.SetValueAsync(expectedValue);
-
-        //    //Assert.That(choiceControl.GetValueAsync, Is.EqualTo(expectedValue));
-        //}
+            Assert.That(await dialog.IsVisibleAsync(), Is.False);
+        }
 
         /// <summary>
-        /// Sets up an error dialog scenario.
+        /// Tests that <see cref="ISetStateDialog.ActivateAsync"/> closes the dialog when it is visible.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task ActivateAsync_DialogIsVisible_Closes()
+        {
+            var dialog = await this.SetupActivateDialogScenarioAsync();
+
+            await dialog.ActivateAsync();
+
+            Assert.That(await dialog.IsVisibleAsync(), Is.False);
+        }
+
+        /// <summary>
+        /// Tests that <see cref="ISetStateDialog.SetValueAsync"/> returns an exception if the record is configured with a single status reason for the given state.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task SetValueAsync_SingleStatusReason_ThrowsException()
+        {
+            var dialog = await this.SetupActivateDialogScenarioAsync();
+
+            Assert.ThrowsAsync<PowerPlaywrightException>(
+                () => dialog.SetValueAsync(pp_record_statecode.Active.ToDisplayName()));
+        }
+
+        /// <summary>
+        /// Tests that <see cref="ISetStateDialog.DeactivateAsync"/> closes the dialog when it is visible.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task SetValueAsync_ContainsValue_ReplacesValue()
+        {
+            var dialog = await this.SetupDeactivateDialogScenarioAsync();
+
+            var expectedValue = pp_record_statuscode.Cancelled.ToDisplayName();
+
+            await dialog.SetValueAsync(expectedValue);
+
+            Assert.That(await dialog.GetValueAsync(), Is.EqualTo(expectedValue));
+        }
+
+        /// <summary>
+        /// Sets up a deactivate dialog scenario.
         /// </summary>
         private async Task<ISetStateDialog> SetupDeactivateDialogScenarioAsync(Faker<pp_Record>? withRecord = null)
         {
-            var record = withRecord ?? new Faker<pp_Record>().Generate();
+            var record = withRecord ?? new Faker<pp_Record>();
 
-            var recordPage = await this.LoginAndNavigateToRecordAsync(record);
+            var recordPage = await this.LoginAndNavigateToRecordAsync(record.Generate());
 
             await recordPage.Form.CommandBar.ClickCommandAsync("Deactivate");
+
+            return recordPage.SetStateDialog;
+        }
+
+        /// <summary>
+        /// Sets up an activate dialog scenario.
+        /// </summary>
+        private async Task<ISetStateDialog> SetupActivateDialogScenarioAsync(Faker<pp_Record>? withRecord = null)
+        {
+            var record = withRecord ?? new Faker<pp_Record>()
+                .RuleFor(x => x.statecode, pp_record_statecode.Inactive)
+                .RuleFor(x => x.statuscode, pp_record_statuscode.Cancelled);
+
+            var recordPage = await this.LoginAndNavigateToRecordAsync(record.Generate());
+
+            await recordPage.Form.CommandBar.ClickCommandAsync("Activate");
 
             return recordPage.SetStateDialog;
         }
