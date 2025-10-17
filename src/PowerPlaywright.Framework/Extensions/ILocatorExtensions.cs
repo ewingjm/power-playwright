9263a1a67bb696ad860a4aecc6a4c1b3581a1e3d
Iain Lomax <iain.lomax@capgemini.com>
2025-10-17 11:17:46 +0100

    feat: add extension to check the existance of an element for a given locator

---
+++ src/PowerPlaywright.Framework/Extensions/ILocatorExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace PowerPlaywright.Framework.Extensions
+{
+    using System.Threading.Tasks;
+    using Microsoft.Playwright;
+
+    /// <summary>
+    /// Extensions for the <see cref="ILocatorExtensions"/>.
+    /// </summary>
+    public static class ILocatorExtensions
+    {
+        /// <summary>
+        /// Checks if the given locator points to at least one existing element.
+        /// </summary>
+        /// <param name="locator">The elemnent locator.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task<bool> ElementExistsAsync(this ILocator locator)
+        {
+            var count = await locator.CountAsync();
+            return count > 0;
+        }
+    }
+}
