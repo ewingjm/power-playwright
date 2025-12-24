f5aeb6fc8d329cc52c0f6a024ba037d2e080ab81
Iain Lomax <iain.lomax@capgemini.com>
2025-12-24 11:42:07 +0000

    test: editable grid integration tests

---
+++ tests/PowerPlaywright.IntegrationTests/Controls/Pcf/IGridControlTests.cs
@@ -0,0 +1,43 @@
+﻿namespace PowerPlaywright.IntegrationTests.Controls.Pcf
+{
+    using Bogus;
+    using PowerPlaywright.Framework.Controls.Pcf.Classes;
+    using PowerPlaywright.TestApp.Model;
+    using PowerPlaywright.TestApp.Model.Fakers;
+
+    /// <summary>
+    /// Tests for the <see cref="IEditableGridControl"/> control class.
+    /// </summary>
+    public partial class IGridControlTests : IntegrationTests
+    {
+        private Faker faker;
+
+        /// <summary>
+        /// Sets up the lookup control.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.faker = new Faker("en_GB");
+        }
+
+        /// <summary>
+        /// Tests that <see cref="IEditableGridControl.OpenRecordAsync(int)"/> opens the record when called with an index that is in range.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task OpenRecordAsync_IndexInRange_OpensRecord()
+        {
+            var gridControl = await this.SetupReadOnlyGridScenarioAsync(withRelatedRecords: [new RelatedRecordFaker()]);
+
+            //var recordPage = await gridControl.OpenRecordAsync(0);
+
+            //await this.Expect(recordPage.Page).ToHaveURLAsync(RelatedRecordFormUrlRegex());
+        }
+
+        private async Task<IEditableGridControl> SetupReadOnlyGridScenarioAsync(Faker<pp_Record>? withRecord = null, IEnumerable<Faker<pp_RelatedRecord>>? withRelatedRecords = null, IEnumerable<Faker<pp_RelatedRecord>>? withRelatableRecords = null)
+        {
+            return null;
+        }
+    }
+}
