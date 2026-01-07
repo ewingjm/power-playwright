82dad667082f505595b8e4ee58c8ecb972fd079e
Iain Lomax <iain.lomax@capgemini.com>
2026-01-07 17:17:33 +0000

    test: add OpenRecordAsync tests

--- tests/PowerPlaywright.IntegrationTests/Controls/Pcf/IGridControlTests.cs
+++ tests/PowerPlaywright.IntegrationTests/Controls/Pcf/IGridControlTests.cs
@@ -1,12 +1,15 @@
 ﻿namespace PowerPlaywright.IntegrationTests.Controls.Pcf
 {
+    using System.Text.RegularExpressions;
     using Bogus;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Messages;
     using PowerPlaywright.Framework.Controls.Pcf.Classes;
     using PowerPlaywright.TestApp.Model;
     using PowerPlaywright.TestApp.Model.Fakers;
 
     /// <summary>
-    /// Tests for the <see cref="IEditableGridControl"/> control class.
+    /// Tests for the <see cref="IEditableGrid"/> control class.
     /// </summary>
     public partial class IGridControlTests : IntegrationTests
     {
@@ -21,23 +24,59 @@
             this.faker = new Faker("en_GB");
         }
 
+
         /// <summary>
-        /// Tests that <see cref="IEditableGridControl.OpenRecordAsync(int)"/> opens the record when called with an index that is in range.
+        /// Tests that <see cref="IEditableGrid.OpenRecordAsync(int)"/> opens the record when called with an index that is in range.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
         [Test]
         public async Task OpenRecordAsync_IndexInRange_OpensRecord()
         {
-            var gridControl = await this.SetupReadOnlyGridScenarioAsync(withRelatedRecords: [new RelatedRecordFaker()]);
+            var gridControl = await this.SetupEditableGridScenarioAsync(withRelatedRecords: [new RelatedRecordFaker()]);
 
-            //var recordPage = await gridControl.OpenRecordAsync(0);
+            var recordPage = await gridControl.OpenRecordAsync(0);
 
-            //await this.Expect(recordPage.Page).ToHaveURLAsync(RelatedRecordFormUrlRegex());
+            await this.Expect(recordPage.Page).ToHaveURLAsync(RelatedRecordFormUrlRegex());
         }
 
-        private async Task<IEditableGridControl> SetupReadOnlyGridScenarioAsync(Faker<pp_Record>? withRecord = null, IEnumerable<Faker<pp_RelatedRecord>>? withRelatedRecords = null, IEnumerable<Faker<pp_RelatedRecord>>? withRelatableRecords = null)
+        /// <summary>
+        /// Tests that <see cref="IEditableGrid.OpenRecordAsync(int)"/> throws a <see cref="IndexOutOfRangeException"/> when the index is out of range.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Test]
+        public async Task OpenRecordAsync_IndexOutOfRange_ThrowsIndexOutOfRangeException()
         {
-            return null;
+            var gridControl = await this.SetupEditableGridScenarioAsync(withRelatedRecords: null);
+
+            Assert.ThrowsAsync<IndexOutOfRangeException>(() => gridControl.OpenRecordAsync(1));
+        }
+
+        [GeneratedRegex(".*pagetype=entityrecord&etn=pp_relatedrecord.*")]
+        private static partial Regex RelatedRecordFormUrlRegex();
+
+        private async Task<IEditableGrid> SetupEditableGridScenarioAsync(Faker<pp_Record>? withRecord = null, IEnumerable<Faker<pp_RelatedRecord>>? withRelatedRecords = null, IEnumerable<Faker<pp_RelatedRecord>>? withRelatableRecords = null)
+        {
+            withRecord ??= new RecordFaker();
+
+            if (withRelatedRecords != null && withRelatedRecords.Any())
+            {
+                withRecord.RuleFor(r => r.pp_Record_RelatedRecord, f => withRelatedRecords?.Select(f => f.Generate()));
+            }
+
+            if (withRelatableRecords != null)
+            {
+                using var client = this.GetServiceClient();
+
+                await client.ExecuteAsync(
+                    new CreateMultipleRequest
+                    {
+                        Targets = new EntityCollection([.. withRelatableRecords.Select(f => f.Generate())]),
+                    });
+            }
+
+            var recordPage = await this.LoginAndNavigateToRecordAsync(withRecord.Generate());
+
+            return recordPage.Form.GetDataSet(pp_Record.Forms.Information.RelatedRecordsSubgrid).GetControl<IEditableGrid>();
         }
     }
 }
