ee85cf58627288b24b89c1d118e00793a3dd186d
Iain Lomax <iain.lomax@capgemini.com>
2026-01-09 17:14:30 +0000

    feat: add column sort order types

---
+++ src/PowerPlaywright.Framework/Model/ColumnSortOrder.cs
@@ -0,0 +1,18 @@
+﻿namespace PowerPlaywright.Framework.Model
+{
+    /// <summary>
+    /// A sort order for a view column.
+    /// </summary>
+    public enum ColumnSortOrder
+    {
+        /// <summary>
+        /// A column sorted in ascending order.
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// A column sorted in descending order.
+        /// </summary>
+        Descending,
+    }
+}
