97ff796877b9ace51ecb33931f09051c5ae762e3
Iain Lomax <iain.lomax@capgemini.com>
2026-02-17 14:55:40 +0000

    feat: add view name constants for pp_RelatedRecord

---
+++ tests/solution/PowerPlaywright.TestApp.Model/Views/pp_relatedrecord.cs
@@ -0,0 +1,21 @@
+﻿namespace PowerPlaywright.TestApp.Model
+{
+    public partial class pp_RelatedRecord
+    {
+        /// <summary>
+        /// Constants relating to views.
+        /// </summary>
+        public static class Views
+        {
+            /// <summary>
+            /// The display name of the Active Records view.
+            /// </summary>
+            public const string ActiveRelatedRecords = "Active Related Records";
+
+            /// <summary>
+            /// The display name of the Inactive Records view.
+            /// </summary>
+            public const string InactiveRelatedRecords = "Inactive Related Records";
+        }
+    }
+}
