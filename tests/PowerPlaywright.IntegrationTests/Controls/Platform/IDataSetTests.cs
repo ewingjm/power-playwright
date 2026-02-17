717399404a37e5f54c202aa117bb059acebea9de
Iain <iainlomax_81@hotmail.com>
2026-02-17 17:28:19 +0000

    fix: DataSet methods broken for subgrids (#131)

--- tests/PowerPlaywright.IntegrationTests/Controls/Platform/IDataSetTests.cs
+++ tests/PowerPlaywright.IntegrationTests/Controls/Platform/IDataSetTests.cs
@@ -16,25 +16,30 @@
         /// <summary>
         /// Tests that calling <see cref="IDataSet.SwitchViewAsync"/> switches the active view when the view selector is visible and the view exists.
         /// </summary>
+        /// <param name="pageType">The type of form.</param>
+        /// <param name="viewName">Expected view name.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Test]
-        public async Task SwitchViewAsync_ViewSelectorVisibleAndViewExists_SwitchesView()
+        [TestCase(typeof(IEntityRecordPage), pp_RelatedRecord.Views.InactiveRelatedRecords)]
+        [TestCase(typeof(IEntityListPage), pp_Record.Views.InactiveRecords)]
+        public async Task SwitchViewAsync_ViewSelectorVisibleAndViewExists_SwitchesView(Type pageType, string viewName)
         {
-            var dataSet = await this.SetupDataSetScenarioAsync();
+            var dataSet = await this.SetupDataSetScenarioAsync(pageType);
 
-            await dataSet.SwitchViewAsync(pp_Record.Views.InactiveRecords);
+            await dataSet.SwitchViewAsync(viewName);
 
-            Assert.That(dataSet.GetActiveViewAsync, Is.EqualTo(pp_Record.Views.InactiveRecords));
+            Assert.That(dataSet.GetActiveViewAsync, Is.EqualTo(viewName));
         }
 
         /// <summary>
         /// Tests that calling <see cref="IDataSet.SwitchViewAsync"/> throws a <see cref="PowerPlaywrightException"/> when the view selector is visible but the view doesn't exists.
         /// </summary>
+        /// <param name="pageType">The type of form.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Test]
-        public async Task SwitchViewAsync_ViewSelectorVisibleButViewDoesNotExist_ThrowsPowerPlaywrightException()
+        [TestCase(typeof(IEntityRecordPage))]
+        [TestCase(typeof(IEntityListPage))]
+        public async Task SwitchViewAsync_ViewSelectorVisibleButViewDoesNotExist_ThrowsPowerPlaywrightException(Type pageType)
         {
-            var dataSet = await this.SetupDataSetScenarioAsync();
+            var dataSet = await this.SetupDataSetScenarioAsync(pageType);
 
             Assert.ThrowsAsync<PowerPlaywrightException>(() => dataSet.SwitchViewAsync("A view that doesn't exist"));
         }
@@ -42,16 +47,35 @@
         /// <summary>
         /// Tests that calling <see cref="IDataSet.GetActiveViewAsync"/> returns the active view when the view selector is visible.
         /// </summary>
+        /// <param name="pageType">The type of form.</param>
+        /// <param name="viewName">Expected view name.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Test]
-        public async Task GetActiveViewAsync_ViewSelectorVisible_ReturnsActiveView()
+        [TestCase(typeof(IEntityRecordPage), pp_RelatedRecord.Views.ActiveRelatedRecords)]
+        [TestCase(typeof(IEntityListPage), pp_Record.Views.ActiveRecords)]
+        public async Task GetActiveViewAsync_ViewSelectorVisible_ReturnsActiveView(Type pageType, string viewName)
         {
-            var dataSet = await this.SetupDataSetScenarioAsync();
+            var dataSet = await this.SetupDataSetScenarioAsync(pageType);
 
-            Assert.That(dataSet.GetActiveViewAsync, Is.EqualTo(pp_Record.Views.ActiveRecords));
+            Assert.That(dataSet.GetActiveViewAsync, Is.EqualTo(viewName));
         }
 
-        private async Task<IDataSet> SetupDataSetScenarioAsync(IEnumerable<Faker<pp_Record>>? withRecords = null)
+        private async Task<IDataSet> SetupDataSetScenarioAsync(Type pageType)
+        {
+            return pageType == typeof(IEntityRecordPage)
+                ? await this.SetupDataSetScenarioRecordPage()
+                : await this.SetupDataSetScenarioListPage();
+        }
+
+        private async Task<IDataSet> SetupDataSetScenarioRecordPage(Faker<pp_Record>? withRecord = null)
+        {
+            withRecord ??= new RecordFaker();
+
+            var recordPage = await this.LoginAndNavigateToRecordAsync(withRecord.Generate());
+
+            return recordPage.Form.GetDataSet(pp_Record.Forms.Information.RelatedRecordsSubgrid);
+        }
+
+        private async Task<IDataSet> SetupDataSetScenarioListPage(IEnumerable<Faker<pp_Record>>? withRecords = null)
         {
             withRecords ??= [new RecordFaker()];
 
