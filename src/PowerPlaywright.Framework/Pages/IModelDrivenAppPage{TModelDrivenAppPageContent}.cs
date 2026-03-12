3a217e9915711b91d635d56a2212b7cef530195d
Iain Lomax <iain.lomax@capgemini.com>
2026-03-12 15:57:11 +0000

    feat: add generic interface for strongly typed model-driven app pages

---
+++ src/PowerPlaywright.Framework/Pages/IModelDrivenAppPage{TModelDrivenAppPageContent}.cs
@@ -0,0 +1,17 @@
+﻿namespace PowerPlaywright.Framework.Pages
+{
+    using PowerPlaywright.Framework.Controls.Platform;
+
+    /// <summary>
+    /// Model-driven app page.
+    /// </summary>
+    /// <typeparam name="TModelDrivenAppPageContent">The type of content contained within the page.</typeparam>
+    public interface IModelDrivenAppPage<out TModelDrivenAppPageContent> : IModelDrivenAppPage
+        where TModelDrivenAppPageContent : IModelDrivenAppPageContent
+    {
+        /// <summary>
+        /// Gets the page content.
+        /// </summary>
+        TModelDrivenAppPageContent Content { get; }
+    }
+}
