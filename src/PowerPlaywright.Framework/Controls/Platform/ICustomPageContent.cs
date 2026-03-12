29de02d30e0581f22225d9fb5c9ec16dc315d46b
Iain Lomax <iain.lomax@capgemini.com>
2026-03-12 16:27:57 +0000

    feat: add model-driven app page content interfaces
    - custom page
    - webresource
    - dashboard

---
+++ src/PowerPlaywright.Framework/Controls/Platform/ICustomPageContent.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerPlaywright.Framework.Controls.Platform
+{
+    /// <summary>
+    /// The contents of a custom page.
+    /// </summary>
+    public interface ICustomPageContent : IModelDrivenAppPageContent
+    {
+    }
+}
---
+++ src/PowerPlaywright.Framework/Controls/Platform/IWebResourcePageContent.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerPlaywright.Framework.Controls.Platform
+{
+    /// <summary>
+    /// The contents of a webresource.
+    /// </summary>
+    public interface IWebResourcePageContent : IModelDrivenAppPageContent
+    {
+    }
+}
