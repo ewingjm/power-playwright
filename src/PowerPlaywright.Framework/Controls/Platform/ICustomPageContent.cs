a8dd9cc795e5f9d8d470440ae7a9ed11ebd9ed2c
Iain Lomax <iain.lomax@capgemini.com>
2026-03-14 09:33:42 +0000

    feat: add navigation page content interfaces
    - add xml documentation

--- src/PowerPlaywright.Framework/Controls/Platform/ICustomPageContent.cs
+++ src/PowerPlaywright.Framework/Controls/Platform/ICustomPageContent.cs
@@ -1,8 +1,11 @@
 ﻿namespace PowerPlaywright.Framework.Controls.Platform
 {
+    using PowerPlaywright.Framework.Controls.Platform.Attributes;
+
     /// <summary>
     /// The contents of a custom page.
     /// </summary>
+    [PlatformControl]
     public interface ICustomPageContent : IModelDrivenAppPageContent
     {
     }
--- src/PowerPlaywright.Framework/Controls/Platform/IWebResourcePageContent.cs
+++ src/PowerPlaywright.Framework/Controls/Platform/IWebResourcePageContent.cs
@@ -1,8 +1,11 @@
 ﻿namespace PowerPlaywright.Framework.Controls.Platform
 {
+    using PowerPlaywright.Framework.Controls.Platform.Attributes;
+
     /// <summary>
     /// The contents of a webresource.
     /// </summary>
+    [PlatformControl]
     public interface IWebResourcePageContent : IModelDrivenAppPageContent
     {
     }
