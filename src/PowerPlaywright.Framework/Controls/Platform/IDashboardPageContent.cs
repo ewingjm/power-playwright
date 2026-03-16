d41c2c096e5a6847b031aa02898b166526241b95
Iain Lomax <iain.lomax@capgemini.com>
2026-03-16 10:34:58 +0000

    fix: add missing platformcontrol attribute

--- src/PowerPlaywright.Framework/Controls/Platform/IDashboardPageContent.cs
+++ src/PowerPlaywright.Framework/Controls/Platform/IDashboardPageContent.cs
@@ -1,8 +1,11 @@
 ﻿namespace PowerPlaywright.Framework.Controls.Platform
 {
+    using PowerPlaywright.Framework.Controls.Platform.Attributes;
+
     /// <summary>
     /// The contents of a dashboard page.
     /// </summary>
+    [PlatformControl]
     public interface IDashboardPageContent : IModelDrivenAppPageContent
     {
     }
