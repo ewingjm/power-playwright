ca8c286b9cec13c57116c1bd616c7f72835f84b9
Iain Lomax <iain.lomax@capgemini.com>
2026-04-07 11:07:27 +0100

    feat: add model-driven app page content type interfaces

---
+++ src/PowerPlaywright.Framework/Controls/Platform/IModelDrivenAppPageContent.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerPlaywright.Framework.Controls.Platform
+{
+    /// <summary>
+    /// The content area of the model-driven app page.
+    /// </summary>
+    public interface IModelDrivenAppPageContent : IPlatformControl
+    {
+    }
+}
---
+++ src/PowerPlaywright.Framework/Controls/Platform/IWebResourcePageContent.cs
@@ -0,0 +1,12 @@
+﻿namespace PowerPlaywright.Framework.Controls.Platform
+{
+    using PowerPlaywright.Framework.Controls.Platform.Attributes;
+
+    /// <summary>
+    /// The contents of a webresource.
+    /// </summary>
+    [PlatformControl]
+    public interface IWebResourcePageContent : IModelDrivenAppPageContent
+    {
+    }
+}
